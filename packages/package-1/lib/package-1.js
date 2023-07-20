7561d9acbd4e98f697e5a854528306eb9d29aa6e
fperez <fperez>
2023-07-20 15:51:48 -0300

    lerna init

---
+++ packages/package-1/lib/package-1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = package1;
+
+function package1() {
+  console.log("package 1");
+  return 'Hello from package1';
+}
+
+package1();
---
+++ packages/package-2/lib/package-2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = package2;
+
+function package2() {
+  console.log("package 2");
+  return 'Hello from package2';
+  
+}
+
+package2();
