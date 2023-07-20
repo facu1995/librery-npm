7942a2efa6cbb29e8ec5b729c069b4dbd4f9410e
fperez <fperez>
2023-07-20 16:02:30 -0300

    cambio package 1

--- packages/package-1/__tests__/package-1.test.js
+++ packages/package-1/__tests__/package-1.test.js
@@ -3,5 +3,5 @@
 const package1 = require('..');
 const assert = require('assert').strict;
 
-assert.strictEqual(package1(), 'Hello from package1');
+assert.strictEqual(package1(), 'Hello from package1 v1');
 console.info('package1 tests passed');
