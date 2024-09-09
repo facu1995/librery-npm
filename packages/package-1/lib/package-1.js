37eda11f02abe054859f9ee2201d308503384ab3
fperez <fperez>
2024-09-09 16:15:24 -0300

    packa1

--- packages/package-1/lib/package-1.js
+++ packages/package-1/lib/package-1.js
@@ -3,7 +3,7 @@
 module.exports = package1;
 
 function package1() {
-  console.log("pacssskage v.1aaaasdasdasd");
+  console.log("pacssskage v.1aaaasdsadasdsadasasdasdasd");
   return 'Hello from package111';
 }
 
